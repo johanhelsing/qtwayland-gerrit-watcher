48b964a2dc4584ed71459143e1f6e8dcf28f32db
Johan Klokkhammer Helsing <johan.helsing@qt.io>
2018-04-24 09:59:20 +0200

    Fix parseVersion for x.x.0 releases
    It was returning patch === undefined previously

--- parseVersion.js
+++ parseVersion.js
@@ -3,12 +3,13 @@ function parseVersion(versionString) {
     const match = versionString.match(versionStringRe);
     const major = parseInt(match[1]);
     const minor = parseInt(match[2]);
-    const patch = parseInt(match[4]) || undefined;
+    const patch = match[3] ? parseInt(match[4]) : undefined;
     return { major: major, minor: minor, patch: patch};
 }
 
 module.exports = parseVersion;
 
+//console.log(parseVersion("5.10.0"));
 //console.log(parseVersion("5.10.1"));
 //console.log(parseVersion("v5.10.1"));
 //console.log(parseVersion("5.10"));
